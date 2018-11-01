eb665595567203f23178fbdd36a3418c59e14ef6
zhiyuanchen <loveuzhiyuan@gmail.com>
2018-11-01 14:05:42 +0800

    JenkinsTest

---
+++ src/main/java/com/example/JenkinsTest/Jenkins/JenkinsApplication.java
@@ -0,0 +1,12 @@
+package com.example.JenkinsTest.Jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsApplication.class, args);
+	}
+}
